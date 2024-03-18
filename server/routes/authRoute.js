d30e6c0d220c833ac3b259135efe11aa6e5cd27b
jayeshbhole7 <jayubhole7@gmail.com>
2024-03-18 11:35:40 +0530

    false

--- server/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const authController =require();
-
-router.post('/register',authController.register);
-router.post('/login',authController.login);
-
-export default router;
--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-
-const route=express.Router();
-
-const userController=require();
-
-const authMiddleware=require();
-
-route.get('/profile',authMiddleware.authenticateToken,userController.get)
